204957a196ca12e61e2ecc12b5f4d7f9399b9ed9
SavinaDV <gvozdareva.darya@mail.ru>
2023-06-29 16:28:00 +0300

    Отредактировала тайм аут.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ from selene import browser
 def browser_management():
 
     browser.config.base_url = 'https://demoqa.com/automation-practice-form'
-    browser.config.timeout = 3.0
+    browser.config.timeout = 4.0
 
     yield
 
