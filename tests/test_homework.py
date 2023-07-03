9287385c948d2b1aa23ccc7f27d9f1e752ed2b80
SavinaDV <gvozdareva.darya@mail.ru>
2023-07-03 17:27:35 +0300

    Добавила шаги кейса

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -1,4 +1,6 @@
-from selene import browser, have, be
+import os
+
+from selene import browser, have, be, by
 
 def test_homework1 ():
     browser.open('/')
@@ -6,6 +8,11 @@ def test_homework1 ():
     browser.element('#firstName').type('Darya')
     browser.element('[placeholder="Last Name"]').type('Savina')
     browser.element('[id = "userEmail"]').type('DaryaSavina@mail.ru')
+    browser.all('#genterWrapper .custom-control').element_by(have.exact_text('Female')).click()
+    browser.element('#userNumber').type('79270371323')
+
+    browser.element(by.id('dateOfBirthInput')).click()
+
 
 
 
